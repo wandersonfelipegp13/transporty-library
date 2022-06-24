78cedfe733a09eefa276ffbd13dde8da8e0c42a1
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-06-24 20:48:05 -0300

    Created DTOs and RabbitMQConstants

---
+++ src/main/java/dto/ProductDto.java
@@ -0,0 +1,11 @@
+package dto;
+
+import java.io.Serializable;
+
+public class ProductDto implements Serializable {
+	public String id;
+	public String name;
+	public String sender;
+	public String receiver;
+	public double weight;
+}
---
+++ src/main/java/dto/ReportDto.java
@@ -0,0 +1,11 @@
+package dto;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ReportDto implements Serializable {
+	public String id;
+	public String productId;
+	public String enterprise;
+	public Date deliveryDate;
+}
