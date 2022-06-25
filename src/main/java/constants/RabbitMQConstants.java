ab117ddb41d4e54d757a4432ecd73cd92e3c691f
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-06-25 14:36:49 -0300

    Added RESPONSE queue

--- src/main/java/constants/RabbitMQConstants.java
+++ src/main/java/constants/RabbitMQConstants.java
@@ -1,9 +1,9 @@
 package constants;
 
-
 public class RabbitMQConstants {
 	public static final String QUEUE_SEND_PRODUCTS  = "SEND_PRODUCTS";
 	public static final String QUEUE_REPORT         = "REPORT";
 	public static final String QUEUE_HEAVY_PRODUCTS = "HEAVY_PRODUCTS";
 	public static final String QUEUE_LIGHT_PRODUCST = "LIGHT_PRODUCTS";
+	public static final String QUEUE_RESPONSE       = "RESPONSE";
 }
