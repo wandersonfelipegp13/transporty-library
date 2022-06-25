fdf4ef003d28682d8a6870653f59e867f91c1b36
Wanderson Felipe <wandersonfelipeplus13@gmail.com>
2022-06-25 15:26:21 -0300

    Date type change to string

--- src/main/java/dto/ReportDto.java
+++ src/main/java/dto/ReportDto.java
@@ -1,11 +1,10 @@
 package dto;
 
 import java.io.Serializable;
-import java.util.Date;
 
 public class ReportDto implements Serializable {
 	public String id;
 	public String productId;
 	public String enterprise;
-	public Date deliveryDate;
+	public String deliveryDate;
 }
